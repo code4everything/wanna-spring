88f869a8b1adb21a8516d38d4d55af7c7fd8f851
zhazhapan <tao@zhazhapan.com>
2018-09-09 12:32:29 +0800

    initialize this project

---
+++ spring-bee/src/test/java/org/code4everything/springbee/SpringBeeApplicationTests.java
@@ -0,0 +1,16 @@
+package org.code4everything.springbee;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBeeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
