b93242fc8a1f49eb0be84207e54595643e0239ee
zhazhapan <tao@zhazhapan.com>
2018-09-09 17:34:23 +0800

    add todo domain

---
+++ spring-bee/src/main/java/org/code4everything/springbee/domain/Todo.java
@@ -0,0 +1,44 @@
+package org.code4everything.springbee.domain;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+/**
+ * @author pantao
+ * @since 2018/9/9
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ApiModel(value = "todo", description = "待办事项")
+public class Todo {
+
+    @Id
+    @ApiModelProperty("事项编号")
+    private String id;
+
+    @ApiModelProperty("用户编号（敏感数据）")
+    private String userId;
+
+    @ApiModelProperty("计划事项完成的日期")
+    private Date doingDate;
+
+    @ApiModelProperty("事项内容")
+    private String content;
+
+    @ApiModelProperty("创建时间")
+    private Timestamp createTime;
+
+    @ApiModelProperty(value = "状态：0待完成，1已完成", allowableValues = "0, 1")
+    private String status;
+
+    @ApiModelProperty("实际完成时间")
+    private Timestamp doneTime;
+}
