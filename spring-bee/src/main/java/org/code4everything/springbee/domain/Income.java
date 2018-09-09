7a611c1f2907912570e0d721718d3f0c088e8579
zhazhapan <tao@zhazhapan.com>
2018-09-09 19:36:32 +0800

    add income domain

---
+++ spring-bee/src/main/java/org/code4everything/springbee/domain/Income.java
@@ -0,0 +1,52 @@
+package org.code4everything.springbee.domain;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.sql.Timestamp;
+
+/**
+ * @author pantao
+ * @since 2018/9/9
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ApiModel(value = "income", description = "收益明细，包括支出")
+public class Income {
+
+    @Id
+    @ApiModelProperty("收益编号")
+    private String id;
+
+    @ApiModelProperty("资产编号")
+    private String assetId;
+
+    @ApiModelProperty(value = "类型：-1支出，1收入", allowableValues = "-1, 1")
+    private Integer type;
+
+    @ApiModelProperty(value = "付款方式（1其他，2支付宝，3微信，4银联，5信用卡，6现金）", allowableValues = "0, 1, 2, 3, 4, 5, 6")
+    private String way;
+
+    @ApiModelProperty("年份")
+    private Integer year;
+
+    @ApiModelProperty("月份")
+    private Integer month;
+
+    @ApiModelProperty("号数")
+    private Integer day;
+
+    @ApiModelProperty("金额")
+    private Long money;
+
+    @ApiModelProperty("备注")
+    private String remark;
+
+    @ApiModelProperty("创建时间")
+    private Timestamp createTime;
+}
