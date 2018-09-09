40f287119708372f18fa90fc8981fdcc7fa1aa22
zhazhapan <tao@zhazhapan.com>
2018-09-09 21:33:18 +0800

    config this project

--- spring-bee/src/main/java/org/code4everything/springbee/SpringBeeApplication.java
+++ spring-bee/src/main/java/org/code4everything/springbee/SpringBeeApplication.java
@@ -1,12 +1,41 @@
 package org.code4everything.springbee;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
+/**
+ * @author pantao
+ * @since 2018/9/9
+ */
 @SpringBootApplication
+@EnableSwagger2Doc
 public class SpringBeeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBeeApplication.class, args);
+    }
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        return corsConfiguration;
+    }
+
+    /**
+     * 跨域过滤器
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
     }
 }
