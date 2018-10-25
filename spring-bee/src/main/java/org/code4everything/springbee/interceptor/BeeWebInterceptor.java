a1c1d0672fe7516b767e8c2b150fa0dce5327d08
pantao <tao@zhazhapan.com>
2018-10-25 14:12:24 +0800

    add web interceptor

---
+++ spring-bee/src/main/java/org/code4everything/springbee/interceptor/BeeWebInterceptor.java
@@ -0,0 +1,52 @@
+package org.code4everything.springbee.interceptor;
+
+import cn.hutool.core.util.StrUtil;
+import com.zhazhapan.util.Checker;
+import org.apache.log4j.Logger;
+import org.code4everything.springbee.constant.BeeConfigConsts;
+import org.code4everything.springbee.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author pantao
+ * @since 2018/10/25
+ **/
+@Component
+public class BeeWebInterceptor implements HandlerInterceptor {
+
+    private final Logger logger = Logger.getLogger(BeeWebInterceptor.class);
+
+    private final RedisTemplate<String, User> userRedisTemplate;
+
+    @Autowired
+    public BeeWebInterceptor(RedisTemplate<String, User> userRedisTemplate) {
+        this.userRedisTemplate = userRedisTemplate;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String url = request.getServletPath();
+        // 白名单，不进行拦截
+        if (Checker.startsWith(url, BeeConfigConsts.WHITE_LIST_PREFIX)) {
+            return true;
+        }
+        // 当URL匹配映射路径并且不是登录页面时进行拦截
+        if (Checker.startsWith(url, BeeConfigConsts.BLACK_LIST_PREFIX)) {
+            String token = request.getHeader("token");
+            User user = userRedisTemplate.opsForValue().get(Checker.checkNull(token));
+            if (Checker.isNull(user)) {
+                // 非法用户，禁止访问
+                logger.error(StrUtil.format("auth error, uuid: {}, ip: {}", token, request.getRemoteAddr()));
+                request.getRequestDispatcher("/error/auth").forward(request, response);
+                return false;
+            }
+        }
+        return true;
+    }
+}
