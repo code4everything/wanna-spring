7d7f28ced894c30c42a50fe16d93c46bfcf4d088
pantao <tao@zhazhapan.com>
2018-10-25 14:50:07 +0800

    test user service api

---
+++ spring-bee/src/test/java/org/code4everything/springbee/constant/TestConsts.java
@@ -0,0 +1,20 @@
+package org.code4everything.springbee.constant;
+
+/**
+ * @author pantao
+ * @since 2018/10/25
+ **/
+public class TestConsts {
+
+    public static final String EMAIL = "tao@zhazhapan.com";
+
+    public static final String PASSWORD = "123456";
+
+    public static final String USERNAME = "pantao";
+
+    public static final String GENDER = "1";
+
+    public static final String BIO = "这是一条测试信息";
+
+    public static final String NICKNAME = "ease";
+}
--- spring-bee/src/test/java/org/code4everything/springbee/service/impl/UserServiceImplTest.java
+++ spring-bee/src/test/java/org/code4everything/springbee/service/impl/UserServiceImplTest.java
@@ -1,13 +1,22 @@
 package org.code4everything.springbee.service.impl;
 
+import com.zhazhapan.util.encryption.JavaEncrypt;
+import org.code4everything.springbee.constant.TestConsts;
+import org.code4everything.springbee.dao.UserDAO;
+import org.code4everything.springbee.domain.User;
 import org.code4everything.springbee.model.RegisterDTO;
+import org.code4everything.springbee.model.UserInfoDTO;
 import org.code4everything.springbee.service.UserService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * You must test method register before any others test
+ */
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class UserServiceImplTest {
@@ -15,29 +24,46 @@ public class UserServiceImplTest {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserDAO userDAO;
+
     @Test
     public void updateInfo() {
+        User user = userDAO.getByEmail(TestConsts.EMAIL);
+        UserInfoDTO userInfoDTO = new UserInfoDTO();
+        userInfoDTO.setBio(TestConsts.BIO);
+        userInfoDTO.setGender(TestConsts.GENDER);
+        userInfoDTO.setNickname(TestConsts.NICKNAME);
+        assert userService.updateInfo(user, userInfoDTO);
     }
 
+    /**
+     * must run this method at first, unless the value is already exists
+     */
     @Test
     public void register() {
         RegisterDTO registerDTO = new RegisterDTO();
-        registerDTO.setEmail("tao@zhahzapan.com");
-        registerDTO.setPassword("123456");
-        registerDTO.setUsername("pantao");
+        registerDTO.setEmail(TestConsts.EMAIL);
+        registerDTO.setPassword(TestConsts.PASSWORD);
+        registerDTO.setUsername(TestConsts.USERNAME);
         userService.register(registerDTO);
     }
 
     @Test
     public void resetPassword() {
-        userService.resetPassword("tao@zhazhapan.com", "123456");
+        userService.resetPassword(TestConsts.EMAIL, TestConsts.PASSWORD);
+        Assert.assertEquals(JavaEncrypt.MD5.digestHex(TestConsts.PASSWORD),
+                userDAO.getByEmail(TestConsts.EMAIL).getPassword());
     }
 
     @Test
     public void updatePassword() {
+        User user = userDAO.getByEmail(TestConsts.EMAIL);
+        assert userService.updatePassword(user, TestConsts.PASSWORD, TestConsts.PASSWORD);
     }
 
     @Test
     public void login() {
+        Assert.assertNotNull(userService.login(TestConsts.EMAIL, TestConsts.PASSWORD));
     }
 }
