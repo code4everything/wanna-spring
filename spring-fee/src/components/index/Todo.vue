f54998d859fef7a743cb24c9bc205ee32004f933
pantao <tao@zhazhapan.com>
2018-10-26 15:02:27 +0800

    finish todo module

--- spring-fee/src/components/index/Todo.vue
+++ spring-fee/src/components/index/Todo.vue
@@ -13,7 +13,8 @@
               <label></label>
             </div>
           </div>
-          <input type="text" class="border-0 bg-light todo-item w-75" v-model="content" :placeholder="todoTip"/>
+          <input type="text" class="border-0 bg-light todo-item w-75" v-model="content" :placeholder="todoTip"
+                 @keyup.enter="saveTodo"/>
         </div>
       </div>
       <!--任务列表-->
@@ -26,7 +27,8 @@
             </div>
           </div>
           <input type="text" :class="['border-0 bg-light todo-item w-75',todo.status==='1'?'deleted':'']"
-                 v-model="todo.content"/>
+                 v-model="todo.content" @blur="updateTodo"/>
+          <a href="javascript:" class="text-danger" @click="remove" v-html="isMobile?removeIcon:removeTip"></a>
         </div>
       </div>
       <br/>
@@ -35,14 +37,27 @@
 </template>
 
 <script>/* eslint-disable */
+import utils from '../../assets/js/utils'
+import validator from '../../../static/js/validator.min'
+import layer from '../../../static/js/layer'
+import {
+  requestListTodo,
+  requestRemoveTodo,
+  requestSaveTodo,
+  requestToggleTodoStatus,
+  requestUpdateTodo
+} from '../../api/api'
 
 export default {
   name: 'Todo',
   data () {
     return {
+      removeIcon: '<i class="glyphicon glyphicon-trash"></i>',
+      removeTip: '删除',
       todoTip: '添加任务',
       content: '',
       idPrefix: 'todo-item-',
+      isMobile: false,
       defaultTodo: {content: '测试', createTime: '', doingDate: '', doneTime: '', id: '', status: '0'},
       todos: []
     }
@@ -53,21 +68,76 @@ export default {
       let src = window.event.srcElement
       let index = $(src).parents('div.todo').attr('data-index')
       let status = src.checked ? '1' : '0'
-      this.todos[index].status = status
+      requestToggleTodoStatus(this.todos[index].id, status).then(data => {
+        if (data.code === 200) {
+          this.todos[index].status = status
+        } else {
+          src.checked = false
+          layer.alert(data.message)
+        }
+      })
+    },
+    remove: function () {
+      let self = this
+      let index = $(window.event.srcElement).parents('div.todo').attr('data-index')
+      layer.confirm('是否确定删除索引位置位于 “' + (parseInt(index) + 1) + '” 的待办事项', {
+        btn: ['确定', '取消']
+      }, function () {
+        layer.load(1)
+        requestRemoveTodo(self.todos[index].id).then(data => {
+          layer.closeAll()
+          if (data.code === 200) {
+            self.todos.splice(index, 1)
+          } else {
+            layer.alert(data.message)
+          }
+        })
+        layer.closeAll()
+      })
+    },
+    saveTodo: function () {
+      if (validator.isEmpty(this.content)) {
+        layer.alert('数据不能为空')
+      } else {
+        layer.load(1)
+        requestSaveTodo(this.date, this.content).then(data => {
+          layer.closeAll()
+          if (data.code === 200) {
+            this.content = ''
+            this.todos.unshift(data.data)
+          } else {
+            layer.alert(data.message)
+          }
+        })
+      }
+    },
+    updateTodo: function () {
+      let index = $(window.event.srcElement).parents('div.todo').attr('data-index')
+      if (validator.isEmpty(this.todos[index].content)) {
+        layer.alert('数据不能为空')
+      } else {
+        requestUpdateTodo(this.todos[index].id, this.todos[index].content).then(data => {
+          if (data.code !== 200) {
+            layer.alert(data.message)
+          }
+        })
+      }
     }
   },
   mounted: function () {
-    this.todos = [{content: '测试', createTime: '', doingDate: '', doneTime: '', id: '', status: '0'}, {
-      content: '测试',
-      createTime: '',
-      doingDate: '',
-      doneTime: '',
-      id: '',
-      status: '1'
-    }]
+    this.isMobile = utils.isMobile()
   },
   watch: {
-    date: function (newValue) {
+    date: function () {
+      layer.load(1)
+      requestListTodo(this.date).then(data => {
+        layer.closeAll()
+        if (data.code === 200) {
+          this.todos = data.data
+        } else {
+          layer.alert(data.message)
+        }
+      })
     }
   },
   updated: function () {
