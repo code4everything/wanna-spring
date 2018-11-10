46aaafa9aa89cfa61110f216770475a1b131a8cb
pantao <tao@zhazhapan.com>
2018-11-10 16:33:15 +0800

    更新保存日志信息

--- spring-bee/src/main/java/org/code4everything/springbee/log/LogAspect.java
+++ spring-bee/src/main/java/org/code4everything/springbee/log/LogAspect.java
@@ -1,23 +1,20 @@
 package org.code4everything.springbee.log;
 
-import cn.hutool.core.util.RandomUtil;
-import com.zhazhapan.util.AopLogUtils;
-import com.zhazhapan.util.Checker;
-import com.zhazhapan.util.LoggerUtils;
-import com.zhazhapan.util.interfaces.SimpleLogService;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.code4everything.boot.constant.MessageConsts;
+import org.code4everything.boot.log.AopLogUtils;
+import org.code4everything.boot.service.LogService;
 import org.code4everything.springbee.domain.Log;
-import org.code4everything.springbee.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -28,25 +25,20 @@ import java.util.concurrent.TimeUnit;
 @Component
 public class LogAspect {
 
+    private final LogService<Log> logLogService;
+
     private final HttpServletRequest request;
 
-    private final SimpleLogService<Log> logService;
-
-    private final RedisTemplate<String, Log> logRedisTemplate;
-
-    private final RedisTemplate<String, User> userRedisTemplate;
-
     @Autowired
-    public LogAspect(HttpServletRequest request, SimpleLogService<Log> logService,
-                     RedisTemplate<String, Log> logRedisTemplate, RedisTemplate<String, User> userRedisTemplate) {
+    public LogAspect(LogService<Log> logLogService, HttpServletRequest request) {
+        this.logLogService = logLogService;
         this.request = request;
-        this.logService = logService;
-        this.logRedisTemplate = logRedisTemplate;
-        this.userRedisTemplate = userRedisTemplate;
     }
 
-    @Pointcut("@annotation(com.zhazhapan.util.annotation.AopLog)")
-    public void serviceAspect() {}
+    @Pointcut("@annotation(org.code4everything.boot.annotations.AopLog)")
+    public void serviceAspect() {
+        // do nothing
+    }
 
     /**
      * 记录用户操作
@@ -58,43 +50,12 @@ public class LogAspect {
         doAfterThrowing(joinPoint, null);
     }
 
-    @AfterThrowing(pointcut = "serviceAspect()", throwing = "e")
-    public void doAfterThrowing(JoinPoint joinPoint, Throwable e) {
-        String className = joinPoint.getTarget().getClass().getName();
-        String methodName = joinPoint.getSignature().getName();
-        Object[] args = joinPoint.getArgs();
-        String description = AopLogUtils.getDescriptionNoThrow(className, methodName, args);
-        User user = userRedisTemplate.opsForValue().get(String.valueOf(request.getAttribute("token")));
-        String uid = Checker.isNull(user) ? "anonymous" : user.getId();
-        String ip = request.getRemoteAddr();
-        Log log;
-        String key = "log:" + uid;
-        if (Checker.isNull(e)) {
-            log = logService.save(getLog(uid, ip, className, methodName, description, args));
-        } else {
-            log = logRedisTemplate.opsForValue().get(key);
-            if (Checker.isNull(log)) {
-                log = getLog(uid, ip, className, methodName, description, args);
-            }
-            log = logService.saveException(log, e.getClass().getName(), e.getMessage());
-        }
-        if (Checker.isNotNull(log)) {
-            logRedisTemplate.opsForValue().set(key, log, 1, TimeUnit.MINUTES);
-            LoggerUtils.info(log.toString());
-        }
-    }
-
-    private Log getLog(String userId, String ip, String className, String methodName, String description,
-                       Object[] args) {
-        Log log = new Log();
-        log.setId(RandomUtil.simpleUUID());
-        log.setArgs(Arrays.toString(args));
-        log.setClassName(className);
-        log.setMethodName(methodName);
-        log.setCreateTime(System.currentTimeMillis());
-        log.setIp(ip);
-        log.setDescription(description);
-        log.setUserId(userId);
-        return log;
+    @AfterThrowing(pointcut = "serviceAspect()", throwing = "throwable")
+    public void doAfterThrowing(JoinPoint joinPoint, Throwable throwable) {
+        // 定义日志缓存
+        Cache<String, Log> cache = CacheBuilder.newBuilder().expireAfterWrite(5, TimeUnit.SECONDS).build();
+        // 定义缓存键
+        String key = request.getHeader(MessageConsts.TOKEN_EN) + Thread.currentThread().getId();
+        AopLogUtils.saveLog(logLogService, cache, key, joinPoint, throwable);
     }
 }
--- spring-bee/src/main/java/org/code4everything/springbee/service/impl/LogServiceImpl.java
+++ spring-bee/src/main/java/org/code4everything/springbee/service/impl/LogServiceImpl.java
@@ -1,23 +1,38 @@
 package org.code4everything.springbee.service.impl;
 
-import com.zhazhapan.util.Checker;
-import com.zhazhapan.util.interfaces.SimpleLogService;
+import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.ObjectUtil;
+import org.code4everything.boot.bean.LogBean;
+import org.code4everything.boot.service.LogService;
 import org.code4everything.springbee.dao.LogDAO;
 import org.code4everything.springbee.domain.Log;
+import org.code4everything.springbee.domain.User;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author pantao
  * @since 2018/10/14
  **/
 @Service
-public class LogServiceImpl implements SimpleLogService<Log> {
+public class LogServiceImpl implements LogService<Log> {
 
     private final LogDAO logDAO;
 
+    private final HttpServletRequest request;
+
+    private final RedisTemplate<String, User> userRedisTemplate;
+
     @Autowired
-    public LogServiceImpl(LogDAO logDAO) {this.logDAO = logDAO;}
+    public LogServiceImpl(LogDAO logDAO, HttpServletRequest request, RedisTemplate<String, User> userRedisTemplate) {
+        this.logDAO = logDAO;
+        this.request = request;
+        this.userRedisTemplate = userRedisTemplate;
+    }
 
     @Override
     public Log save(Log log) {
@@ -25,12 +40,24 @@ public class LogServiceImpl implements SimpleLogService<Log> {
     }
 
     @Override
-    public Log saveException(Log log, String exceptionClass, String exceptionDetail) {
-        if (Checker.isNotNull(log)) {
-            log.setExceptionClass(exceptionClass);
-            log.setExceptionDetail(exceptionDetail);
+    public Log saveException(Log log, Throwable throwable) {
+        if (ObjectUtil.isNotNull(log) && ObjectUtil.isNotNull(throwable)) {
+            log.setExceptionClass(throwable.getClass().getName());
+            log.setExceptionDetail(throwable.getMessage());
             logDAO.save(log);
         }
+        return log;
+    }
+
+    @Override
+    public Log getLog(LogBean logBean) {
+        Log log = new Log();
+        BeanUtils.copyProperties(logBean, log);
+        log.setId(IdUtil.simpleUUID());
+        log.setCreateTime(System.currentTimeMillis());
+        log.setIp(request.getRemoteAddr());
+        User user = userRedisTemplate.opsForValue().get(String.valueOf(request.getAttribute("token")));
+        log.setUserId(ObjectUtil.isNull(user) ? "anonymous" : user.getId());
         return log;
     }
 }
