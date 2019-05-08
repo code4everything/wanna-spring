c82e5cf67c4a6b4ca7080a6aacfb2d69862bef5e
pantao <easepan@qq.com>
2019-05-08 17:22:55 +0800

    更新异常类

--- spring-bee/src/main/java/org/code4everything/springbee/exception/JobNotFoundException.java
+++ spring-bee/src/main/java/org/code4everything/springbee/exception/JobNotFoundException.java
@@ -1,15 +1,15 @@
 package org.code4everything.springbee.exception;
 
-import org.code4everything.boot.web.mvc.exception.HttpOkException;
+import org.code4everything.boot.web.mvc.exception.HttpException;
 import org.springframework.http.HttpStatus;
 
 /**
  * @author pantao
  * @since 2019/3/1
  **/
-public class JobNotFoundException extends HttpOkException {
+public class JobNotFoundException extends HttpException {
 
     public JobNotFoundException() {
-        super(HttpStatus.NOT_FOUND.value(), "指定的工作日志不存在");
+        super(HttpStatus.NOT_FOUND.value(), "指定的工作日志不存在", true);
     }
 }
