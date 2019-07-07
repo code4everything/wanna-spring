db7af4b015bfcfe5a8f17aab505982722c8503a2
pantao <tao@zhazhapan.com>
2019-07-07 21:02:59 +0800

    fix: warning

--- spring-bee/src/main/java/org/code4everything/springbee/config/BeeConfigBean.java
+++ spring-bee/src/main/java/org/code4everything/springbee/config/BeeConfigBean.java
@@ -1,19 +1,17 @@
 package org.code4everything.springbee.config;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.code4everything.boot.web.mvc.FilterPath;
 
 /**
  * @author pantao
  * @since 2018/10/28
  */
-@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
 public class BeeConfigBean extends FilterPath {
 
     private Integer tokenExpired;
